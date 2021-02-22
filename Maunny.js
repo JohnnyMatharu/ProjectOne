d73e60e3856ba7a08a13cee48eedc9415c8478e5
Johnny <jaani.matharu@gmail.com>
2021-02-22 16:01:20 -0500

    last updated Feb 22 2021

---
+++ Maunny.js
@@ -0,0 +1,20 @@
+var Buy= ["New", "Used"]
+var Year= [2021, 2020, 2019]//and so on no strings
+var Make and Model= [
+    {"GM": ["Suburban", "Terrain", "Acadia"],
+"Dodge": ["RAM", "Durango", "Challenger"], 
+"Ford"[]; // connect all models
+    }] 
+// and so on, don't forget to connect all this, add function 
+//Write a function which takes an array item as arguments. click on make array choices then produce model arrays, for GM/Make[0] connect Model[0]
+var Engine= []//connect this to make as input
+var Drive= ["Two Wheel" "Four Wheel"]; //connect this with drive 
+var Shape= ["Coupe", "Sedan", "SUV", "2 door", "4 door"];//add more
+var Fuel= ["diesal", "gasoline"];//this could be connected to certain models
+var Special Accessories= ["Trailer Hitch", "carrier", "tinted windows" ]//add more
+var Colour= ["Barcelona Red", "Black"]//connect this to number of colours avaialable in that model in that year
+//GOOD LUCK Johnny
+
+
+
+asd
